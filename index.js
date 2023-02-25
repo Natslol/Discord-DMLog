da77d7d774e88f008a10ec257776fd4b29a1cbfa
Kumalala <askipmaildeouf@protonmail.com>
2023-02-26 00:06:44 +0100

    group dm and attachments 🥰

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ client.on('ready', async () => {
 })
 
 client.on('messageDelete', (deletedMessage) => {
-    if (deletedMessage.channel.type === "DM")
+    if (deletedMessage.channel.type === "DM") {
         if(deletedMessage.author.id !== client.user.id)
             console.log(`\n[${deletedMessage.author.tag}] ${deletedMessage.content}`);
+            if(deletedMessage.attachments) {
+                for (let file of deletedMessage.attachments.values()) {
+                    console.log(`[↳] ${file.url}`);
+                }
+            }
+    } else if (deletedMessage.channel.type === "GROUP_DM") {
+        if(deletedMessage.author.id !== client.user.id) {
+            console.log(`\n[${deletedMessage.author.tag}] ${deletedMessage.content}`);
+            if(deletedMessage.attachments) {
+                for (let file of deletedMessage.attachments.values()) {
+                    console.log(`[↳] ${file.url}`);
+                }
+            }
+        }
+    }
 })
 
 client.login(process.env.YOUR_TOKEN);
